ed19dd37a7994df5c64f3ee0f0d14be03fe96ea8
Whippie37 <whippiewolf@gmail.com>
2026-02-14 01:49:58 -0500

    Did most of mobile styling

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function goToSection(sectionNum) {
+    window.scrollTo({top: document.getElementById("info-section").offsetTop + 70});
     let carouselItems = document.getElementsByClassName('carousel-item');
     for (let i = 0; i<carouselItems.length; i++) {
         carouselItems[i].style.transform = `translateX(-${90*sectionNum}vw)`;
